bde94c28051e7bf127e7b0093f3cc6e19df3d2d6
rami <ramizul127@gmail.com>
2025-09-21 03:31:50 -0400

    auto load entities when registered from within the modules

--- apps/api/src/database/typeorm.config.ts
+++ apps/api/src/database/typeorm.config.ts
@@ -1,6 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
-import { join } from 'path';
 
 export const typeOrmConfigAsync = async (
   config: ConfigService
@@ -11,7 +10,7 @@ export const typeOrmConfigAsync = async (
   username: config.get<string>('DB_USERNAME'),
   password: config.get<string>('DB_PASSWORD'),
   database: config.get<string>('DB_NAME'),
-  entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')],
+  autoLoadEntities: true,
   synchronize: config.get<string>('DB_SYNC') === 'true',
   logging: config.get<string>('NODE_ENV') !== 'production',
 });
